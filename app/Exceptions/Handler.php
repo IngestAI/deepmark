e503fcdee367a332d12c3afa958171ff2eaecaad
Igor Shaev <igor.shaev@gmail.com>
2023-10-22 09:04:37 +0200

    Hot fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,6 +36,12 @@ class Handler extends ExceptionHandler
             return response()->json([
                 'error' => 'Task not found'
             ], 404);
+        }
+
+        if ($e instanceof ValidationException) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 419);
         }
 
         return parent::render($request, $e);
